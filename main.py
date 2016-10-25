6e950f320dc1e2d7531aa7793bf3c83af425e3d1
renzj <renzju@gmail.com>
2016-10-25 10:25:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#encoding=utf8
+#encoding=utf-8
 #-------------------------------------------------------------------------------
 # Name:
 # Purpose:
 #
-# Author:      tianmingZhang
+# Author:      
 #
 # Created:     06/04/2016
-# Copyright:   (c) tianmingZhang 2016
+# Copyright:   (c)  2016
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
@@ -28,7 +28,7 @@ def read_config(path):
     except Exception,ex:
         isSuccess=False
     finally:
-        if fi !=None :
+        if fi !=None : 
             fi.close()
 
     return isSuccess,fiobj
@@ -47,12 +47,10 @@ def main(config_path="cfbench.json"):
     mcur.execute('select max(respid) from responses')
 
     last_resp_id=mcur.fetchone()
-
-    if len(last_resp_id) == 0:
+    if len(last_resp_id) ==0 or( len(last_resp_id) != 0 and last_resp_id[0]==None) :
         last_resp_id = []
         last_resp_id.append(0)
-
-
+        
     iat_module,iat_name = configuration(config_objs['request_of_iat']['name'],config_objs['request_of_iat']['conf_tag'])
     mIATGenerator=getattr(iat_module,iat_name)(**(config_objs['request_of_iat']['kwargs']))
 
@@ -77,19 +75,17 @@ def main(config_path="cfbench.json"):
     mRequestGenerator.runGenerator()
     endtime = time.time()
     mcur.execute('insert into analysis(tid,disruptive,sleep_for_excess,'\
-                 'quota,threads_over_quota,total_generate_times,number_of_request_per_time,run_time,begintime,endtime,last_resp_id) values' \
-                 '(%d,"%s",%d,%d,%d,%d,%d,"%s","%s","%s",%d)' %\
+                 'quota,threads_over_quota,total_generate_time,requests_per_make,threads_of_pool,run_time,begintime,endtime,last_resp_id) values' \
+                 '(%d,"%s",%d,%d,%d,%d,%d,%d,"%s","%s","%s",%d)' %\
                  (config_objs['tenant']['tenantid'],str(config_objs['tenant']['disruptive']),config_objs['tenant']['sleep_for_excess'],\
-                 config_objs['tenant']['tenant_quota'],config_objs['tenant']['threads_over_quota'],config_objs['request_generator']['kwargs']['total_generate_times'],\
-                 config_objs['request_generator']['kwargs']['number_of_request_per_time'],str(endtime - btime),str(btime),str(endtime),last_resp_id[0]))
-
+                 config_objs['tenant']['tenant_quota'],config_objs['tenant']['threads_over_quota'],config_objs['request_generator']['kwargs']['total_generate_time'],\
+                 config_objs['request_generator']['kwargs']['request_per_make'],config_objs['request_generator']['kwargs']['threads_of_pool'],str(endtime - btime),\
+                 str(btime),str(endtime),last_resp_id[0]))
     db_conn.commit()
 
     mysql_utils.closeConn(db_conn,mcur)
 
     return
-
-
 def testPop(**kwargs):
     mlist=[]
     for i in xrange(20):
@@ -103,11 +99,3 @@ def testPop(**kwargs):
 
 if __name__ == '__main__':
     main()
-##    kws={"total_generate_times":1000,"number_of_request_per_time":100,"check_time":5}
-##    kaa=kws
-##    kaa.update({'count':123})
-##    print kaa
-##    testPop(**kaa)
-
-
-
