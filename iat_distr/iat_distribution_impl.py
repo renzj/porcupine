c6921ae2d6e386b38fe559cc422d34845566b2d1
renzj <renzju@gmail.com>
2016-10-25 10:09:24 +0800

    Update iat_distribution_impl.py

--- iat_distr/iat_distribution_impl.py
+++ iat_distr/iat_distribution_impl.py
@@ -2,10 +2,10 @@
 # Name:
 # Purpose:
 #
-# Author:      tianmingZhang
+# Author:      
 #
 # Created:     18/04/2016
-# Copyright:   (c) tianmingZhang 2016
+# Copyright:   (c)  2016
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import  random
@@ -13,38 +13,29 @@ import  random
 class IATGenerator(object):
 
     def __init__(self,**kwargs):
-        pass
+        self.seed= kwargs.get('seed',1)
+
+        random.seed(self.seed)
+        self.time_base = kwargs.get('time_base',10)
 
     def Get(self):
         pass
-
-class ConstantIATGenerator(IATGenerator):
-
-    def __init__(self,**kwargs):
-
-        IATGenerator.__init__(self,**kwargs)
-        self.constant_time = kwargs.get("contant_time",1)
-    def Get(self):
-
-        return self.constant_time
 
 class ParetoIATGenerator(IATGenerator):
 
     def __init__(self,**kwargs):
 
         IATGenerator.__init__(self,**kwargs)
-
         self.alpha = kwargs.get('alpha',1)
-        self.seed= kwargs.get('seed',1)
-        random.seed(self.seed)
-        self.time_base = kwargs.get('time_base',10)
 
     def Get(self):
 
-        return random.paretovariate(self.alpha) / self.time_base # BASE:1, 10 ,100 ,
+        return random.paretovariate(self.alpha) / self.time_base,self.time_base # BASE:1, 10 ,100 ,
 
 def main():
     pass
 
 if __name__ == '__main__':
+    main()
+
     main()
